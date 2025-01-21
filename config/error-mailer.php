bd72ea5a81692382f5f00824ad9f0a2fe8ec7b0f
Wellington Cavalcante <wellarts@gmail.com>
2025-01-21 13:58:07 -0300

    error

---
+++ config/error-mailer.php
@@ -0,0 +1,31 @@
+<?php
+
+return [
+    'email' => [
+        'recipient' => ['wellarts@gmail.com'],
+        'bcc' => [],
+        'cc' => [],
+        'subject' => 'ERRO EM SISTEMA - ' . env('APP_NAME'),
+    ],
+
+    'disabledOn' => [
+        //
+    ],
+
+    'cacheCooldown' => 10, // in minutes
+
+    'webhooks' => [
+        'discord' => env('ERROR_MAILER_DISCORD_WEBHOOK'),
+
+        'message' => [
+            'title' => 'Error Alert - ' . env('APP_NAME'),
+            'description' => 'ERRO EM SISTEMA.',
+            'error' => 'Error',
+            'file' => 'File',
+            'line' => 'Line',
+            'details_link' => 'See more details'
+        ],
+    ],
+
+    'storage_path' => storage_path('app/errors'),
+];
